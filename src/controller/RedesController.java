51ba058292a17e00f280063edd0e686fc9964832
wil <wil@PC-Wil>
2018-08-24 10:59:27 -0300

    Criação do primeiro método da atividade.

---
+++ src/controller/RedesController.java
@@ -0,0 +1,24 @@
+package controller;
+
+public class RedesController {
+	
+	public String ip (String nomeSo){
+		
+		String Linux = "Linux";
+		String Windows = "Windows";
+		
+		if (nomeSo.equals(Linux)){
+		
+			return nomeSo;
+		
+		} else if (nomeSo.equals(Windows)){
+			
+			return nomeSo;
+		
+		}else {return "Algo deu errado !";}
+		
+		
+
+}
+	
+}
---
+++ src/view/Main.java
@@ -0,0 +1,18 @@
+package view;
+
+import javax.swing.JOptionPane;
+
+import controller.RedesController;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		RedesController rdc = new RedesController();
+		String nomeSo = System.getProperty("os.name");
+		
+		System.out.println(rdc.ip(nomeSo));
+
+	}
+
+}
