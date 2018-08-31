5fd58d1a55a58c564fc98e166474da95afccc6fd
wil <wil@PC-Wil>
2018-08-31 11:43:00 -0300

    fim aparente, revisar os metodos de ping e ip.

--- src/controller/RedesController.java
+++ src/controller/RedesController.java
@@ -12,28 +12,42 @@ public class RedesController {
 	
 	public void ip (String nomeSo) throws IOException{ 
 		
+		
 		if (nomeSo.equals("Linux")){
 			
 			try{
 				
-				Process proc = Runtime.getRuntime().exec("ifconfig wlan");
+				
+				Process proc = Runtime.getRuntime().exec("ifconfig eth0");
 				InputStream fluxo = proc.getInputStream();
 				InputStreamReader leitor = new InputStreamReader(fluxo);
 				BufferedReader buffer = new BufferedReader(leitor);
 				String linha = buffer.readLine();
 				
+				String [] vetor = new String [linha.length()];
+				int i = 0;
 				
 				while(linha != null){
-					
+				
+					i++;
 					System.out.println(linha);
 					linha = buffer.readLine();
 					
+					vetor[i] = linha;
+
+					}
+				/*
+				for (i = 0; i < vetor.length; i++){
 					
-				}
-				
-				
-				
-				} catch (IOException e){
+					if (vetor[i] ==  null){
+					
+					vetor[i] = "";
+					
+					} else {System.out.println(vetor[i]);}
+				}*/
+			
+			} 
+				 catch (IOException e){
 					
 					String erro = e.getMessage();
 					JOptionPane.showMessageDialog(null, erro, "ERRO" , JOptionPane.ERROR_MESSAGE);
@@ -68,5 +82,74 @@ public class RedesController {
 			}
 
 		}
+	
+	
+	public void ping (String nomeSo) throws IOException{ 
+		
+		
+		if (nomeSo.equals("Linux")){
+			
+			try{
+				
+				
+				Process proc = Runtime.getRuntime().exec("ping www.google.com");
+				InputStream fluxo = proc.getInputStream();
+				InputStreamReader leitor = new InputStreamReader(fluxo);
+				BufferedReader buffer = new BufferedReader(leitor);
+				String linha = buffer.readLine();
+				
+				
+				
+				while(linha != null){
+				
+					
+					System.out.println(linha);
+					linha = buffer.readLine();
+					
+					
+
+					}
+				
+			
+				
+				
+			
+			} 
+				 catch (IOException e){
+					
+					String erro = e.getMessage();
+					JOptionPane.showMessageDialog(null, erro, "ERRO" , JOptionPane.ERROR_MESSAGE);
+					e.printStackTrace();
+				}
+			
+
+			
+		} else if (nomeSo.equals("Windows 10")){
+			
+			try{
+				
+				Process proc = Runtime.getRuntime().exec("ping www.google.com");
+				InputStream fluxo = proc.getInputStream();
+				InputStreamReader leitor = new InputStreamReader(fluxo);
+				BufferedReader buffer = new BufferedReader(leitor);
+				String linha = buffer.readLine();
+				
+				while(linha != null){
+					
+					System.out.println(linha);
+					linha = buffer.readLine();
+					
+				}
+				
+				} catch (IOException e){
+					
+					String erro = e.getMessage();
+					JOptionPane.showMessageDialog(null, erro, "ERRO" , JOptionPane.ERROR_MESSAGE);
+					e.printStackTrace();
+				}
+			}
+
+		}
+
 		
 	}
--- src/view/Main.java
+++ src/view/Main.java
@@ -13,7 +13,47 @@ public class Main {
 		RedesController rdc = new RedesController();
 		String nomeSo = System.getProperty("os.name"); 
 	
-		rdc.ip(nomeSo);
+		
+		
+		
+		int opcao = 0;
+		
+		do{
+		
+			opcao = Integer.parseInt(
+					JOptionPane.showInputDialog("Menu Processos \n "
+							+ "1- Executar configurações de ip \n"
+							+ "2- Executar configurações de ping \n"
+							+ "9- Fim"));
+			
+			switch (opcao){
+			
+			case 1:
+				
+				rdc.ip(nomeSo);
+				
+				break;
+				
+			case 2:
+				
+				rdc.ping(nomeSo);
+				
+				break;
+				
+			case 9:
+				
+				break;
+				
+				default:
+					
+					System.out.println("Opcao invalida !");
+			
+			}
+			
+			
+		}while(opcao != 9);
+		
+		
 		
 	}
 }
